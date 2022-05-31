6d81ed16cefccf0f9997d1d8cbb59675c1858c9c
Tsien Jin <tsien.jin@gmail.com>
2022-05-31 21:19:59 +0800

    changes

--- hearmeout/pages/index.jsx
+++ hearmeout/pages/index.jsx
@@ -4,8 +4,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return(
+    <>
+    <Head>
+        <title>Hear Me Out</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <div className="flex items-center justify-center h-screen w-screen">
       <h1>Hello world!</h1>
     </div>
+    </>
   )
 }
